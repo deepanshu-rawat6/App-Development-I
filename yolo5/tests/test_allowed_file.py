b5464e91a599ac771c6b90217dc7fd8aafc0313d
deepanshu-rawat6 <deepanshu.dr10@gmail.com>
2023-07-04 15:08:53 +0530

    updated: README.md

--- yolo5/tests/test_allowed_file.py
+++ yolo5/tests/test_allowed_file.py
@@ -1,5 +1,5 @@
 import unittest
-from app import allowed_file, ALLOWED_EXTENSIONS
+from utils import allowed_file, ALLOWED_EXTENSIONS
 
 
 class TestBacklogPerInstanceMetric(unittest.TestCase):
@@ -10,4 +10,3 @@ class TestBacklogPerInstanceMetric(unittest.TestCase):
     def test_invalid_filename(self):
         self.assertFalse(allowed_file(f'file'))
         self.assertFalse(allowed_file(f'file.csv'))
-
